c02b90badbaf2a87323cd276481b90a0a52da35f
Swaganthiesh <42667567+Swaganthiesh@users.noreply.github.com>
2025-06-11 20:16:10 +0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  features {}
+}
+
+# Optionally, you can use environment variables for authentication
+# environment variable AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT_ID, AZURE_SUBSCRIPTION_ID
